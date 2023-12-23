7f937343e819033ceaed78abf7a22dff12e50b0c
C Gan <charactory@users.noreply.github.com>
2023-12-23 17:15:44 +0800

    added minifier

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import nunjucks from 'vite-plugin-nunjucks'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default {
     plugins: [
         nunjucks(),
-    ]
+        ViteMinifyPlugin({}),
+    ],
 }
 
