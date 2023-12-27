9a8063da684af5b1d748f91fad43ca37f14b0991
C Gan <charactory@users.noreply.github.com>
2023-12-27 21:22:01 +0800

    rewrite step1: define problem

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import nunjucks from 'vite-plugin-nunjucks'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
+/*ViteMinifyPlugin({}),*/
 
 export default {
     plugins: [
         nunjucks(),
-        ViteMinifyPlugin({}),
     ],
 }
 
